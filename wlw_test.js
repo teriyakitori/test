493e4e2ae7a1f29c2f3b922b9e555511cf0b3d7e
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-06 02:47:43 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -59,7 +59,7 @@ function sorceget(){
 	
 	if (request.readyState == 4 && request.status == 200){
 		console.log("準備完了1");
-		batdom = dom_parser.parseFromString(request.responseText , "text/html");
+		batdom = dom_parser.parseFromString(request.responseXML , "text/xml");
 		console.log("準備完了2");
 		
 		// 変数にデータを入れていく

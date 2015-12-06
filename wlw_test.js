a0e69640c41427e4efb9ce34c1388ea33ca3a0ad
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-06 20:47:11 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -12,7 +12,7 @@ function sorceget(){
 	if (request.readyState == 4 && request.status == 200){
 		var batdom = request.responseText;
 		$(function() {
-			console.log($(".mtc_detail_map").text());
+			console.log("テスト１：" + $(".mtc_detail_map").text());
 		});
 		console.log( batdom.match(/マリン/i) );
 	}

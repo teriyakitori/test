0c9f1dbfc0e5ef620cacebe589ef18d44d6e73f1
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-07 00:31:20 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -1,23 +1,18 @@
 javascript:
-
 var request = null;
+var dom_parser = new DOMParser();
 request= new XMLHttpRequest();
-var urlstr = "https://wonderland-wars.net/matchlogdetail.html?id=16919588";
+var urlstr = "https://wonderland-wars.net/matchlogdetail.html?id=16918436";
 
 request.open("GET", urlstr, true);
 request.onreadystatechange=sorceget;
 request.send(null);
 
-function sorceget(){
-	if (request.readyState == 4 && request.status == 200){
-		var batdom = request.responseText;
-		$.get( batdom, function(tmpdata){
-			console.log("てすと１：" + $(".mtc_detail_store").text() );
-		});
-		console.log( batdom.match(/マリン/i) );
-	}
-	else{
-	
-	}
+if (request.readyState == 4 && request.status == 200){
+	var batdom = request.responseText;
+	console.log("test:" + batdom);
+	jQuery(function ($) {
+		var teststr = $(batdom).(".mtc_detail_store").text();
+	});
+	console.log("てすと１：" + teststr)
 }
-alert("処理終了");

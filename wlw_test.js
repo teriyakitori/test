563cd008f4a36a2998c083cd7f4a24b26546c29b
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-07 02:25:36 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -13,9 +13,9 @@ function sorceget(){
 		var teststr = null;
 		var batdom = request.responseText;
 		
-		console.log("テスト：" + request.responseText);
+		// console.log("テスト：" + request.responseText);
 		jQuery(function ($) {
-			$(teststr).load(request.responseText, ".mtc_detail_map");
+			$(teststr).load(urlstr, ".mtc_detail_map");
 		});
 		console.log("てすと１：" + teststr);
 	} else {

3efa526f0bc8a5fce18c12348e2ecbb0df18f063
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-07 19:24:50 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -1,6 +1,7 @@
 javascript:
 var request = null;
 request= new XMLHttpRequest();
+var myHTML = null;
 var urlstr = "https://wonderland-wars.net/matchlogdetail.html?id=16919588";
 
 request.open("GET", urlstr, true);
@@ -11,7 +12,7 @@ function sorceget(){
 	if (request.readyState == 4 && request.status == 200){
 		var batdom = request.responseText;
 		jQuery(function ($) {
-			var myHTML = $.parseHTML(batdom);
+			myHTML = $.parseHTML(batdom);
 		});
 		var batdel = myHTML.getElementsByClassName("mtc_detail_data_heishi");
 		console.log("兵士撃破数：" + batdel[0].innerHTML);

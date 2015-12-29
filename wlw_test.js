e604d9142f47b7b6dd1ab459744116ab7d20ee0c
teriyakitori <hibi_001@hotmail.co.jp>
2015-12-30 00:07:21 +0900

    Update wlw_test.js

--- wlw_test.js
+++ wlw_test.js
@@ -1533,8 +1533,8 @@ function select_fun(getno){
 					lsdata_getidx = parseInt(localStorage.getItem(lsidx_name));
 					lsdata_getold = localStorage.getItem(lsold_name);
 					lsdata_getnew = localStorage.getItem(lsnew_name);
-					if(JSON.parse(localStorage.getItem(lsmap_name)) != null){
-						map_ary = JSON.parse(localStorage.getItem(lsmap_name);
+					if( JSON.parse( localStorage.getItem(lsmap_name) ) != null ){
+						map_ary = JSON.parse(localStorage.getItem(lsmap_name));
 					}
 				} else {
 					// 無かったら0で
